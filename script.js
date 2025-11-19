69dde63149830c7f864d08116396f1f566a3aca5
Basti <sebastian.hedwig@web.de>
2025-11-19 18:18:30 +0100

    changed variable

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ async function hashPassword(password) {
     const data = encoder.encode(password);
     const hashBuffer = await crypto.subtle.digest("SHA-256", data);
     return [...new Uint8Array(hashBuffer)]
-        .map(b => b.toString(16).padStart(2, "0"))
+        .map(byte => byte.toString(16).padStart(2, "0"))
         .join("");
 }
 
